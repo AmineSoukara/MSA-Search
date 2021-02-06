6ddb9b80dff1f515ed5992d78c167a0d3d3607f6
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-02-06 08:59:49 +0100

    Update __init__.py

--- acutebot/__init__.py
+++ acutebot/__init__.py
@@ -28,7 +28,7 @@ if ENV:
     SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
     SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
     DEBUG = bool(os.environ.get("DEBUG", False))
-    ARLTOKEN = os.environ.get("ARLTOKEN")
+    ARLTOKEN = os.environ.get("ARL")
     APP_URL = os.environ.get("APP_URL")
     APIID = os.environ.get("APIID")
     APIHASH = os.environ.get("APIHASH")
@@ -45,7 +45,7 @@ else:
     SPT_CLIENT_SECRET = Config.SPT_CLIENT_SECRET
     SPT_CLIENT_ID = Config.SPT_CLIENT_ID
     DEBUG = Config.DEBUG
-    ARLTOKEN = Config.ARLTOKEN
+    ARLTOKEN = Config.ARL
     APP_URL = Config.APP_URL
     APIID = Config.APIID
     APIHASH = Config.APIHASH
