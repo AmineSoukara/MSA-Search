755c19d4c3ab1c896ff34e4f303d8a86fd8f33ff
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-02-06 08:15:25 +0100

    Update and rename Config.py to config.py

--- acutebot/config.py
+++ acutebot/config.py
@@ -1,7 +1,5 @@
-from acutebot.config import Config
-
 TOKEN = "1669267094:AAEuI84SHn7qhYSILyCSePpBXolaMM_ffd4"
-WORKERS = 8
+WORKERS = "8"
 TMDBAPI = "32aee9e12d269ed374ce682cd6a022d0"
 DB_URI = "mongodb+srv://gwenstephanie:need4speed@cluster0-4tpcj.mongodb.net/test?retryWrites=true&w=majority"
 GENIUS = "P5W9LglHPgaJAQGWg1ncmK6VIUsxIRObX8wK6Bva9HSEPUHcP7ZXTkUiQBMTA9U5"
