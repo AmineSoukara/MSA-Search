58400730a6acf2f92e71e85e7b96877380f70dbd
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-02-06 08:24:38 +0100

    Update config.py

--- acutebot/config.py
+++ acutebot/config.py
@@ -4,15 +4,15 @@ from telegram.ext import Updater, Defaults
 from telegram import ChatAction, ParseMode
 from acutebot.config import Config
 
-TOKEN = Config.TOKEN
-WORKERS = Config.WORKERS
-TMDBAPI = Config.TMDBAPI
-DB_URI = Config.DB_URI
-GENIUS = Config.GENIUS
-SPT_CLIENT_SECRET = Config.SPT_CLIENT_SECRET
-SPT_CLIENT_ID = Config.SPT_CLIENT_ID
-DEBUG = Config.DEBUG
-ARLTOKEN = Config.ARL
-APP_URL = Config.APP_URL
-APIID = Config.APIID
-APIHASH = Config.APIHASH
+TOKEN = os.environ.get("TOKEN")
+WORKERS = int(os.environ.get("WORKERS", 8))
+TMDBAPI = os.environ.get("TMDBAPI")
+DB_URI = os.environ.get("DATABASE_URL")
+GENIUS = os.environ.get("GENIUS")
+SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
+SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
+DEBUG = bool(os.environ.get("DEBUG", False))
+ARLTOKEN = os.environ.get("ARL")
+APP_URL = os.environ.get("APP_URL")
+APIID = os.environ.get("APIID")
+APIHASH = os.environ.get("APIHASH")
